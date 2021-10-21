30548f1d7c5ea818ef6536be1e0d7d67fe2a9e97
charles-mrt <charlesmartins.ads@gmail.com>
2021-10-21 20:04:08 -0300

    function to handle changing social media links

---
+++ js/social-url.js
@@ -0,0 +1,26 @@
+
+
+/**
+ * get social media links 
+ * **/
+
+const LinksSocialMedia = {
+  github: "charles-mrt",
+  youtube: "charles-mrt",
+  facebook: "charles-mrt",
+  instagram: "charles-mrt",
+  twiiter: "charles-mrt"
+}
+
+function changeSocialMediaLinks() {
+
+  for (let li of social_links.children) {
+
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `https://${social}.com/${LinksSocialMedia[social]}`
+
+  }
+
+}
+changeSocialMediaLinks()
